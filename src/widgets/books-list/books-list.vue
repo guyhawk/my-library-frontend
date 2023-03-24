1f5ebe5b86c1911fde8ac385c73889ea700b3d5d
glide <glide@ngs.ru>
2023-03-24 18:49:52 +0700

    fixed books-list widget

--- src/widgets/books-list/books-list.vue
+++ src/widgets/books-list/books-list.vue
@@ -1,17 +1,45 @@
 <script setup lang="ts">
-  import {toRefs} from 'vue';
-  import {Book} from '@/shared/api';
-  import {BookListRow} from '@/entities/books';
 
 
-  interface BooksRowProps {
-    books: Book[];
+  import {onMounted, computed} from 'vue'
+  import {useStore} from 'vuex';
+  import {booksModel} from '@/entities/books';
+  import {categoriesModel} from '@/entities/categories';
+  import { Category } from '@/shared/api';
+
+  const store = useStore()
+
+  const books = computed(()=> store.state['books'].books)
+  const categories = computed(()=> store.state['categories'].categories)
+
+  const getBooksListAsync = () => store.dispatch(booksModel.actions.getBooksListAsync);
+  const getCategoriesAsync = () => store.dispatch(categoriesModel.actions.getCategoriesAsync);
+
+  const tableHead = ['Serial', 'Author', 'Title', 'Category', '']
+  const colunmTemplate = '100px 1fr 2fr 150px 140px'
+
+  onMounted(() => {
+    getBooksListAsync()
+    getCategoriesAsync()
+  })
+
+  const getCategoryName = (value: string) => {
+    if (value) {
+      return 'value'
+    }
+    return ''
   }
-  const props = defineProps<BooksRowProps>()
-  const {books} = toRefs(props)
 
 </script>
 
 <template>
-  <BookListRow v-for="book in books" :book="book" :key="book._id" />
+  <Table :head="tableHead" :colunmTemplate="colunmTemplate">
+    <TableRow :colunmTemplate="colunmTemplate" v-for="book in books" :key="book._id">
+      <TableColumn>{{book.serial}}</TableColumn>
+      <TableColumn>{{book.author}}</TableColumn>
+      <TableColumn>{{book.title}}</TableColumn>
+      <TableColumn>{{getCategoryName(book.category)}}</TableColumn>
+      <TableColumn>button</TableColumn>
+    </TableRow>
+  </Table>
 </template>
