217c2dccc6dac3f0681610c159d4073eb7b7279d
glide <glide@ngs.ru>
2023-03-24 18:48:30 +0700

    added ui element page-title

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,9 +1,15 @@
 import {Button} from './button'
 import {InputText} from './input-text'
-
-
+import {Table} from './table'
+import {TableColumn} from './table-column'
+import {TableRow} from './table-row'
+import {PageTitle} from './page-title'
 
 export default [
   {name: 'Button', value: Button},
   {name: 'InputText', value: InputText},
+  {name: 'Table', value: Table},
+  {name: 'TableColumn', value: TableColumn},
+  {name: 'TableRow', value: TableRow},
+  {name: 'PageTitle', value: PageTitle},  
 ]
---
+++ src/shared/ui/page-title/page-title.vue
@@ -0,0 +1,13 @@
+<template>
+  <dic class="page-title">
+    <slot></slot>
+  </dic>
+</template>
+
+<style lang="scss" scoped>
+  .page-title {
+    font-size: 2rem;
+    border-left: 5px solid var(--primary-color);
+    padding-left: 24px;
+  }
+</style>
